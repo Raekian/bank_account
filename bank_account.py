e72dd34b82685b55e175d548e5e6a3b0ef98868e
Raekian <Stevenzerwas@gmail.com>
2021-06-08 11:09:19 -0700

    part 2 changes

--- bank_account.py
+++ bank_account.py
@@ -1,3 +1,20 @@
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(int_rate=0.02, balance=0)
+
+    def user_deposit(self, x):
+        self.account.deposit(x)
+        print("account balance is", self.account.balance)
+
+    def user_withdraw(self, x):
+        self.account.withdraw(x)
+        print("account balance is", self.account.balance)
+
+    def user_display(self):
+        self.account.display_balance()
+
 class BankAccount:
     all_accounts = []
     def __init__(self, int_rate, balance):
@@ -18,6 +35,9 @@ class BankAccount:
         self.balance -= amount
         return self
 
+    def display_balance(self):
+        print(self.balance)
+
     def display_account_info(self):
         print(self.int_rate, self.balance)
         return self
@@ -27,9 +47,13 @@ class BankAccount:
         return self
 
 
-steven = BankAccount(.02, 6000)
-caleb = BankAccount(.04, 10000)
+steven = User("steven", "Steven@gmail.com")
+caleb = User("caleb", "caleb@gmail.com")
 
-steven.deposit(1000).deposit(1000).deposit(1000).withdraw(3000).yield_interest().display_account_info()
-caleb.deposit(1000).deposit(1000).withdraw(500).withdraw(500).withdraw(500).withdraw(500).yield_interest().display_account_info()
-BankAccount.display_all()
+steven.user_deposit(1000)
+steven.user_withdraw(500)
+steven.user_display()
+
+# steven.deposit(1000).deposit(1000).deposit(1000).withdraw(3000).yield_interest().display_account_info()
+# caleb.deposit(1000).deposit(1000).withdraw(500).withdraw(500).withdraw(500).withdraw(500).yield_interest().display_account_info()
+# BankAccount.display_all()
